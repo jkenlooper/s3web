23bcc0816da1e770d3cf839f7e88c1d491d9cf50
Jake Hickenlooper <jake@weboftomorrow.com>
2012-05-15 11:46:26 -0600

    moved scripty stuff into script.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ execfile('src/s3web/_version.py')
 setup(name="s3web",
     version=__version__,
     author='Jake Hickenlooper',
-    author_email='jake.hickenlooper@gmail.com',
+    author_email='jake@weboftomorrow.com',
     description="Simple upload files to amazon s3 as a website",
     packages=['s3web'],
     package_dir={'': 'src'},
@@ -15,6 +15,6 @@ setup(name="s3web",
     include_package_data = True,
     entry_points=("""
       [console_scripts]
-      s3web=s3web:main
+      s3web=s3web.script:main
       """)
     )
