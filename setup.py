af33dec6bfa753f80e9638fe0d1a67506e908f92
Jake Hickenlooper <jake@weboftomorrow.com>
2013-03-16 08:49:21 -0500

    include readme and update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-execfile('src/s3web/_version.py')
-setup(name="s3web",
-    version=__version__,
+name = "s3web"
+version = "0.1.2"
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name=name,
+    version=version,
     author='Jake Hickenlooper',
     author_email='jake@weboftomorrow.com',
     description="Simple upload files to amazon s3 as a website",
-    packages=['s3web'],
     package_dir={'': 'src'},
-    install_requires=[
-      'boto',
-      'progressbar',
-      ],
+    packages=find_packages('src'),
     include_package_data = True,
-    entry_points=("""
-      [console_scripts]
-      s3web=s3web.script:main
-      """)
-    )
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'boto',
+        'progressbar',
+    ],
+    entry_points="""
+    [console_scripts]
+    s3web = s3web.script:main
+    """,
+)
